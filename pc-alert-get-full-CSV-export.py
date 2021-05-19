73dc9d6579297e0680464b85677bb2a3aa86f8ea
Antoine Sylvia <antoinesylvia@yahoo.com>
2021-05-18 23:26:41 -0500

    Update pc-alert-get-full-CSV-export.py

--- pc-alert-get-full-CSV-export.py
+++ pc-alert-get-full-CSV-export.py
@@ -103,12 +103,12 @@ parser.add_argument(
     default=30,
     help='(Optional) - Time Range in days.  Defaults to 30.')
 
-parser.add_argument(
-    '-l',
-    '--limit',
-    type=int,
-    default=500,
-    help='(Optional) - Return values limit (Default to 500).')
+#parser.add_argument(
+    #'-l',
+    #'--limit',
+    #type=int,
+    #default=500,
+    #help='(Optional) - Return values limit (Default to 500).')
 parser.add_argument(
     '-fagt',
     '--accountgroup',
@@ -168,7 +168,7 @@ alerts_filter['sortBy'] = ["id:asc"]
 
 alerts_filter['offset'] = 0
 
-alerts_filter['limit'] = args.limit
+#alerts_filter['limit'] = args.limit
 
 alerts_filter['filters'] = []
 if args.alertstatus is not None:
